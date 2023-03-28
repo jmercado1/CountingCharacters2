9860b1266a8c9679776a19a69c4b1b0daab98493
jmercado1 <125997850+jmercado1@users.noreply.github.com>
2023-03-28 09:50:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CountingCharacters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string
+            value = "def";
+            foreach (char e in value)
+            {
+                Console.WriteLine(e);
+            }
+            Dictionary<char, int> diclist = new Dictionary<char, int>();
+            Console.WriteLine("Enter character string");
+
+            foreach (var chr in Console.ReadLine())
+            {
+                int count = 0;
+                if (diclist.ContainsKey(chr))
+                {
+                    count = diclist[chr];
+                }
+                diclist[chr] = count + 1;
+            }
+                foreach (KeyValuePair<char, int> pair in diclist)
+                {
+                    Console.WriteLine(pair.Key + " = " + pair.Value);
+                }
+                Console.ReadLine();
+        }
+    }
+}
